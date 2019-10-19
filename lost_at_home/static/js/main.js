fdd2ba8726a9e4070997dfdf7f69bc08c69df014
Khoa Tran <trananh_khoa@outlook.com>
2019-10-19 13:28:57 -0400

    Linked CSS and JS files to main SPA

--- lost_at_home/static/js/main.js
+++ lost_at_home/static/js/main.js
@@ -1,3 +1,25 @@
 // JS file to contain all UI logic
 
-alert('HELLO WORLD!')
+////////////////////////////////////////////////////////////////////////////////
+/* GAME LOGIC                                                                 */
+////////////////////////////////////////////////////////////////////////////////
+
+const TOTAL_GAMES = 10      // Max number of games to win
+const TOTAL_LIVES = 3       // Max number of lives
+
+const SUCCESS_MESSAGE = 'Correct!'
+const FAILURE_MESSAGE = 'Incorrect!'
+const GAME_SUCCESS_MESSAGE = 'Congratulations!'
+const GAME_FAILURE_MESSAGE = 'Better luck next time!'
+
+let gameRounds = 0          // Tracker for how mahy games have been played
+let successfullRounds = 0   // Tracker for how many games have been completed
+let failedRounds = 0        // Tracker for how many games have been failed
+
+////////////////////////////////////////////////////////////////////////////////
+/* LAH MENU                                                                   */
+////////////////////////////////////////////////////////////////////////////////
+
+$('#lah-menu-start-button').on('click', () => {
+    alert('PLAYER IS STARTING THE GAME!')
+})
