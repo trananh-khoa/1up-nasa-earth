dd02599821e73264853f6030d4263e67f085241f
Khoa Tran <trananh_khoa@outlook.com>
2019-10-20 12:27:23 -0400

    Redid javascript logic

--- lost_at_home/static/js/main.js
+++ lost_at_home/static/js/main.js
@@ -11,161 +11,92 @@ const SUCCESS_MESSAGE = 'Correct!'
 const FAILURE_MESSAGE = 'Incorrect!'
 const GAME_SUCCESS_MESSAGE = 'Congratulations!'
 const GAME_FAILURE_MESSAGE = 'Better luck next time!'
-let game_timer = 60
+const GAME_TIME_LIMIT = 60
 
-const GAME_ID = {
-    startbutton: '', // $()
-    question: '',
-    image: '',
-    options1: '',
-    options2: '',
-    options3: '',
-    options4: '',
-    lives: '',
-    timer: ''
+const GAME_IDS = {
+    gameRoundText: $('#lah-minigame-round'),
+    gameQuestionText: $('#lah-minigame-question-text'),
+    startbutton: $('#lah-menu-start-button'),
+    image: $('#lah-minigame-img'),
+    buttons: $('#lah-minigame-options'),
+    options1: $('#lah-optionA'),
+    options2: $('#lah-optionB'),
+    options3: $('#lah-optionC'),
+    options4: $('#lah-optionD'),
+    lives1: $('#lah-lives-1'),
+    lives2: $('#lah-lives-2'),
+    lives3: $('#lah-lives-3'),
+    timerValue: $('#lah-progress-bar-value'),
+    timerBar: $('#lah-progress-bar')
 }
-// HOLDS ALL IDS FOR RELEVANT ELEMENTS
-// QUESTION ID
-// IMAGE ID
-// FOUR BUTTONS ID
-// LIVES ID
-// TIMER PROGRESS BAR ID
 
 let gameRounds = 0          // Tracker for how many games have been played
 let successfullRounds = 0   // Tracker for how many games have been completed
 let failedRounds = 0        // Tracker for how many games have been failed
+let minigameData = ''
+let waitingforUserInput = false
+let minigameChoice = ''
 
-// SEND INITIAL AJAX REQUEST TO GET FIRST MINIGAME
-$.ajax({
-    type: 'GET',
-    url: '/minigame',
-    success: function (data) {
-    },
-    dataType: 'json'
-});
-
-// FOR LOOP ONLY TRIGGERS ONCE START BUTTON IS CLICKED
-function getMinigame() {
+function startGame() {
     for (let i = 0; i < TOTAL_GAMES; i++) {
-        // UPDATE VALUES FOR ROUND (gameRounds + 1 --> ID SPAN)
-        // LOAD (SHOW) THE FIRST MINIGAME
-        // DISPLAY IMAGE (ID IMAGE)
-        $(GAME_ID.image).html(data.image);
-        // DISPLAY QUESTION (ID QUESTIONS)
-        $(GAME_ID.question).html(data.question);
-        // DISPLAY CHOICES (ID BUTTONS)
-        $(GAME_ID.options1).html(data.buttons1);
-        $(GAME_ID.options2).html(data.buttons2);
-        $(GAME_ID.options3).html(data.buttons3);
-        $(GAME_ID.options4).html(data.buttons4);
-        // SEND AJAX FOR NEXT MINIGAME
-        $.ajax({
-            type: 'GET',
-            url: '/api/minigame',
-            success: function (data2) {
-            },
-            dataType: 'json'
-        });
-        // SET TIMER AND LISTEN FOR BUTTON CLICK
-        timer()
-            .addEventListener("click", userTemp = /*userclicked*/);
-        // COMPARE BUTTON CLICK OR TIMER END TO DETERMINE SUCCESS/FAIL
-        if (data.answer === /*userclick*/) {
-            successfullRounds++
-            timerReset()
-        }
-        else {
-            failedRounds++
-            timerReset()
-        }
-        // LOOP AGAIN
+        // Update rounds
+        gameRounds = i + 1
+        GAME_IDS.gameRoundText.text(gameRounds)
 
-        if (failedRounds === TOTAL_LIVES || /*resetButton*/) {
-            break;
+        // Get Minigame Data
+        getMinigameData()
+
+        // Update minigame UI
+        updateMinigameUI(minigameData)
+
+        // Listen for input
+        waitingforUserInput = true
+        while(waitingforUserInput) {
+            console.log('Waiting for answer...')
+            setTimeout(1000)
         }
 
-        // BREAK CONDITIONS
-        // RESET BUTTON
-        // failedRounds === TOTAL_LIVES)
+        // Check answer
+        if (minigameChoice === minigameData.answer) {
+            console.log('Correct answer!')
+        } else {
+            console.log('Failed question!')
+            failedRounds = failedRounds + 1
+        }
+
     }
-
 }
+
+function getMinigameData() {
+    return $.getJSON('/minigame', function(result) {
+        minigameData = $.parse(result)
+        console.log(minigameData)
+    })
+}
+
+function updateMinigameUI(minigameData) {
+    // Update image
+    GAME_IDS.image.attr('src', minigameData.img_path);
+
+    // Update question
+    GAME_IDS.gameQuestionText.text(minigameData.question)
+
+    // Update choices
+    GAME_IDS.options1.text(minigameData.choices[0])
+    GAME_IDS.options2.text(minigameData.choices[1])
+    GAME_IDS.options3.text(minigameData.choices[2])
+    GAME_IDS.options4.text(minigameData.choices[3])
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 /* LAH MENU                                                                   */
 ////////////////////////////////////////////////////////////////////////////////
 
-$('#lah-menu-start-button').on('click', () => {
-    // CHECK ID FOR START BUTTON
-    // STARTS GAME
+GAME_IDS.startbutton.on('click', () => {
+    startGame()
 })
 
-$('#lah-menu-intructions-button').on('click', () => {
-    // CHECK ID FOR MODAL BUTTON
-    // IS THIS EVEN NEEDED?
+GAME_IDS.buttons.find('.btn').on('click', function() {
+    minigameChoice = this.text()
+    waitingforUserInput = false
 })
-
-function GameLogic() {
-    for (var i; i < 11; i++) {
-        getQuestion();
-
-    }
-}
-
-function getMinigame() {
-    $.ajax({
-        type: 'GET',
-        url: '/api/minigame',
-        success: function (data) {
-            var questionsInfo = {
-                question: data.question,
-                image: data.image,
-                options: options.image
-            }
-            var answer = {
-                qAnswer: data.answer
-            }
-            $('tbody').html(questionsInfo);
-            timer()
-            // wait for user click code 
-            // get click answer and store in var
-            var userClick
-            answerValidation(userClick, answer)
-        },
-        dataType: 'json'
-    });
-}
-
-function answerValidation(userAnswer, actualAnswer) {
-    if (userAnswer === actualAnswer) {
-        successfullRounds++;
-        //update user square
-        // get to next question?
-    }
-    else {
-        TOTAL_LIVES--;
-        // update the HTML
-        if (TOTAL_LIVES === 0) {
-            // you lost
-        }
-        else {
-            // get to next question 
-        }
-    }
-}
-
-function timer() {
-    var countDownTimer = setInterval(function () {
-        // need to put this in html <div id="countdown"></div>
-        document.getElementById("countdown").innerHTML = GAME_TIMER + " seconds remaining";
-        GAME_TIMER -= 1;
-        if (GAME_TIMER <= 0) {
-            clearInterval(countDownTimer);
-            document.getElementById("countdown").innerHTML = "you are out of time"
-        }
-    }, 1000);
-}
-
-
-GAME_ID.startbutton.addEventListener("click", getMinigame);
-// startButton.addEventListener("click", );
-submitButton.addEventListener("click", answerValidation);
